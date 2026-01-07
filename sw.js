0c2f046f7f771da25948cc5e56700dff7e9e0695
darklordbrofc <newaplike@gmail.com>
2026-01-07 00:24:04 -0300

    Fix syntax error in service worker file

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
 const CACHE_NAME = 'ponto-elite-v15';
-
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
 });
